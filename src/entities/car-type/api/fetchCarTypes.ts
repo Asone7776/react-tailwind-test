2e15e43da0bc7b1c30dd4cc39f9f5d51dae55074
asone7776 <asone7776@gmail.com>
2025-10-28 00:34:37 +0500

    simplify request

--- src/entities/car-type/api/fetchCarTypes.ts
+++ src/entities/car-type/api/fetchCarTypes.ts
@@ -2,10 +2,6 @@ import { instance } from '@shared/api/instance.ts';
 import { ModelConfig } from '@shared/api/crud/models.ts';
 
 export const fetchCarTypes = async () => {
-  try {
-    const { data } = await instance.get(ModelConfig.car_type.url);
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
+  const { data } = await instance.get(ModelConfig.car_type.url);
+  return data;
 };
